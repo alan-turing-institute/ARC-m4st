2afa41e3c391577046cdc7d5f016057ec4377135
klh5 <k.awtycarroll@turing.ac.uk>
2024-12-03 12:36:43 +0000

    :construction: Adding files for metrics test with DEMETR

---
+++ src/m4st/metrics.py
@@ -0,0 +1,38 @@
+import evaluate
+from nltk.translate.bleu_score import sentence_bleu
+from sonar.inference_pipelines.text import TextToEmbeddingModelPipeline
+from sonar.models.blaser.loader import load_blaser_model
+
+
+def nltk_bleu_score(reference: str, prediction: str) -> float:
+    return sentence_bleu([reference.split()], prediction.split())
+
+
+class SacreBLEUScore:
+    """Loads SacreBLEU from the evaluate library."""
+
+    def __init__(self) -> None:
+        self.bleu = evaluate.load("sacrebleu")
+
+    def get_score(self, reference: str, prediction: str) -> float:
+
+        score = self.bleu.compute(predictions=[prediction], references=[[reference]])
+        return score["score"]
+
+
+class BLASERScore:
+    """Initialises and applies the BLASER 2.0 metric from the SONAR library."""
+
+    def __init__(self) -> None:
+        self.blaser_ref = load_blaser_model("blaser_2_0_ref").eval()
+        self.blaser_qe = load_blaser_model("blaser_2_0_qe").eval()
+        self.text_embedder = TextToEmbeddingModelPipeline(
+            encoder="text_sonar_basic_encoder", tokenizer="text_sonar_basic_encoder"
+        )
+
+    def blaser_score(self, reference: str, prediction: str, source: str) -> float:
+        src_embs = self.text_embedder.predict([source], source_lang="eng_Latn")
+        ref_embs = self.text_embedder.predict([reference], source_lang="eng_Latn")
+        mt_embs = self.text_embedder.predict([prediction], source_lang="eng_Latn")
+
+        return self.blaser_ref(src=src_embs, ref=ref_embs, mt=mt_embs).item()
