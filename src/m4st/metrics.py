374a26a18e6ad92188a52bae32c513440abb28a4
klh5 <k.awtycarroll@turing.ac.uk>
2024-12-06 08:41:55 +0000

    :sparkles: Add COMET metric

--- src/m4st/metrics.py
+++ src/m4st/metrics.py
@@ -9,7 +9,7 @@ def nltk_bleu_score(reference: str, prediction: str) -> float:
 
 
 class SacreBLEUScore:
-    """Loads SacreBLEU from the evaluate library."""
+    """Applies SacreBLEU from the evaluate library."""
 
     def __init__(self) -> None:
         self.bleu = evaluate.load("sacrebleu")
@@ -25,7 +25,7 @@ class BLASERScore:
 
     def __init__(self) -> None:
         self.blaser_ref = load_blaser_model("blaser_2_0_ref").eval()
-        # self.blaser_qe = load_blaser_model("blaser_2_0_qe").eval()
+        self.blaser_qe = load_blaser_model("blaser_2_0_qe").eval()
         self.text_embedder = TextToEmbeddingModelPipeline(
             encoder="text_sonar_basic_encoder", tokenizer="text_sonar_basic_encoder"
         )
@@ -39,3 +39,31 @@ class BLASERScore:
         mt_embs = self.text_embedder.predict([prediction], source_lang="eng_Latn")
 
         return self.blaser_ref(src=src_embs, ref=ref_embs, mt=mt_embs).item()
+
+    def blaser_qe_score(
+        self, prediction: str, source: str, source_lang_code: str
+    ) -> float:
+
+        src_embs = self.text_embedder.predict([source], source_lang=source_lang_code)
+        mt_embs = self.text_embedder.predict([prediction], source_lang="eng_Latn")
+
+        return self.blaser_qa(src=src_embs, mt=mt_embs).item()
+
+
+class COMETScore:
+    """Applies COMET and COMET-QE metrics from the evaluate library."""
+
+    def __init__(self) -> None:
+        self.comet = evaluate.load("comet", model="wmt21-comet-mqm")
+        self.comet_qe = evaluate.load("comet", model="wmt21-comet-qe-mqm")
+
+    def comet_ref_score(self, reference: str, prediction: str, source: str) -> float:
+
+        score = self.comet.compute(
+            predictions=[prediction], references=[reference], sources=[source]
+        )
+        return score["score"]
+
+    def comet_qe_score(self, prediction: str, source: str) -> float:
+        score = self.comet_qe.compute(predictions=[prediction], sources=[source])
+        return score["score"]
