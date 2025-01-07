f311f09711f1ef54893442cfa7228f0d1d5b5fe8
klh5 <k.awtycarroll@turing.ac.uk>
2025-01-07 15:26:06 +0000

    :zap: Batch BLASER scoring by source language

--- src/m4st/metrics.py
+++ src/m4st/metrics.py
@@ -1,4 +1,6 @@
 import evaluate
+import numpy as np
+import pandas as pd
 from nltk.translate.bleu_score import sentence_bleu
 from sonar.inference_pipelines.text import TextToEmbeddingModelPipeline
 from sonar.models.blaser.loader import load_blaser_model
@@ -20,55 +22,111 @@ class SacreBLEUScore:
         return score["score"]
 
 
-class BLASERScore:
-    """Initialises and applies the BLASER 2.0 metric from the SONAR library."""
+class BLASERRefScore:
+    """Initialises and applies the BLASER 2.0 QE metric from the SONAR library."""
 
     def __init__(self) -> None:
         self.blaser_ref = load_blaser_model("blaser_2_0_ref").eval()
+        self.text_embedder = TextToEmbeddingModelPipeline(
+            encoder="text_sonar_basic_encoder", tokenizer="text_sonar_basic_encoder"
+        )
+
+    def get_scores(
+        self,
+        references: pd.Series,
+        predictions: pd.Series,
+        sources: pd.Series,
+        source_lang_codes: pd.Series,
+    ) -> float:
+
+        langs = np.unique(source_lang_codes)
+
+        # Store results for all languages so they can be returned together
+        results = []
+
+        # BLASER requires the source language, so at best we can batch by language as
+        # source_lang must be a string
+        for language in langs:
+            mask = source_lang_codes == language
+            sources_lang = np.array(sources[mask])
+            refs_lang = np.array(references[mask])
+            preds_lang = np.array(predictions[mask])
+
+            src_embs = self.text_embedder.predict(sources_lang, source_lang=language)
+            ref_embs = self.text_embedder.predict(refs_lang, source_lang="eng_Latn")
+            mt_embs = self.text_embedder.predict(preds_lang, source_lang="eng_Latn")
+
+            for i in range(len(src_embs)):
+                result = self.blaser_ref(
+                    src=src_embs[[i]], ref=ref_embs[[i]], mt=mt_embs[[i]]
+                ).item()
+                results.append(result.item())
+
+        return results
+
+
+class BLASERQEScore:
+    """Initialises and applies the BLASER 2.0 reference-based metric from the SONAR
+    library."""
+
+    def __init__(self) -> None:
         self.blaser_qe = load_blaser_model("blaser_2_0_qe").eval()
         self.text_embedder = TextToEmbeddingModelPipeline(
             encoder="text_sonar_basic_encoder", tokenizer="text_sonar_basic_encoder"
         )
 
-    def blaser_ref_score(
-        self, reference: str, prediction: str, source: str, source_lang_code: str
+    def get_scores(
+        self, predictions: pd.Series, sources: pd.Series, source_lang_codes: pd.Series
     ) -> float:
 
-        src_embs = self.text_embedder.predict([source], source_lang=source_lang_code)
-        ref_embs = self.text_embedder.predict([reference], source_lang="eng_Latn")
-        mt_embs = self.text_embedder.predict([prediction], source_lang="eng_Latn")
+        langs = np.unique(source_lang_codes)
 
-        return self.blaser_ref(src=src_embs, ref=ref_embs, mt=mt_embs).item()
+        # Store results for all languages so they can be returned together
+        results = []
 
-    def blaser_qe_score(
-        self, prediction: str, source: str, source_lang_code: str
-    ) -> float:
+        # BLASER requires the source language, so at best we can batch by language as
+        # source_lang must be a string
+        for language in langs:
+            mask = source_lang_codes == language
+            sources_lang = np.array(sources[mask])
+            preds_lang = np.array(predictions[mask])
 
-        src_embs = self.text_embedder.predict([source], source_lang=source_lang_code)
-        mt_embs = self.text_embedder.predict([prediction], source_lang="eng_Latn")
+            src_embs = self.text_embedder.predict(sources_lang, source_lang=language)
+            mt_embs = self.text_embedder.predict(preds_lang, source_lang="eng_Latn")
 
-        return self.blaser_qe(src=src_embs, mt=mt_embs).item()
+            for i in range(len(src_embs)):
+                result = self.blaser_ref(src=src_embs[[i]], mt=mt_embs[[i]]).item()
+                results.append(result.item())
+
+        return results
 
 
-class COMETScore:
-    """Applies COMET and COMET-QE metrics from the evaluate library."""
+class COMETRefScore:
+    """Applies COMET reference-based metric from the evaluate library."""
 
     def __init__(self) -> None:
 
         self.comet = evaluate.load("comet", model="wmt21-comet-mqm")
-        self.comet_qe = evaluate.load("comet", model="wmt21-comet-qe-mqm")
 
-    def comet_ref_score(self, reference: str, prediction: str, source: str) -> float:
+    def get_scores(
+        self, references: pd.Series, predictions: pd.Series, sources: pd.Series
+    ) -> float:
 
         score = self.comet.compute(
-            predictions=[prediction],
-            references=[reference],
-            sources=[source],
+            predictions=predictions,
+            references=references,
+            sources=sources,
         )
-        # Note this only works because we're passing through a single example
-        return score["scores"][0]
+        return score["scores"]
 
-    def comet_qe_score(self, prediction: str, source: str) -> float:
-        score = self.comet_qe.compute(predictions=[prediction], sources=[source])
-        # Single example again
-        return score["scores"][0]
+
+class COMETQEScore:
+    """Applies COMET QE metric from the evaluate library."""
+
+    def __init__(self) -> None:
+
+        self.comet = evaluate.load("comet", model="wmt21-comet-qe-mqm")
+
+    def comet_qe_score(self, predictions: pd.Series, sources: pd.Series) -> float:
+        score = self.comet_qe.compute(predictions=predictions, sources=sources)
+        return score["scores"]
