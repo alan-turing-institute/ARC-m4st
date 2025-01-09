51da636cafbf71658d0b8b663e5e2ad91ade869b
Boyko Vodenicharski <bvodenicharski@turing.ac.uk>
2025-01-09 15:17:30 +0000

    Remove ollama translation test
    * Alternatively would have to get ollama set up in the CI VM, so just removing for now. This is not testing fundamental functionality

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,4 +1,4 @@
-from m4st.translate.model import OllamaTranslateModel, T5TranslateModel
+from m4st.translate.model import T5TranslateModel
 
 eng_text = "Hi, my name is Bob."
 
@@ -9,9 +9,10 @@ def test_t5():
     print(translated_text_t5)
 
 
-def test_llama32():
-    llama32translate = OllamaTranslateModel()
-    translated_text_llama32 = llama32translate(
-        eng_text, source_lang_iso="eng", target_lang_iso="fra"
-    )
-    print(translated_text_llama32)
+# NOTE: Ollama server must be running for this test to work, skipping it for now.
+# def test_llama32():
+#    llama32translate = OllamaTranslateModel()
+#    translated_text_llama32 = llama32translate(
+#        eng_text, source_lang_iso="eng", target_lang_iso="fra"
+#    )
+#    print(translated_text_llama32)
