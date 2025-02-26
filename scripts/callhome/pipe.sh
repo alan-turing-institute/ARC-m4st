39f59c043daa971c5505503ac54aced1721d8162
Boyko Vodenicharski <bvodenicharski@turing.ac.uk>
2025-02-26 10:35:29 +0000

    Add NLLB and update preprocessing
    * The update to preprocessing adds cleaning of the A/B notation for speaker.

--- scripts/callhome/pipe.sh
+++ scripts/callhome/pipe.sh
@@ -12,6 +12,7 @@ SCRIPT1="./align_lines.py"
 SCRIPT2="./remove_noise_syntax.py"
 SCRIPT3="${PATH_TO_FISHER}/data/bin/strip_markup.pl"
 SCRIPT4="${PATH_TO_FISHER}/data/bin/remove_punctuation.pl"
+SCRIPT5="./remove_dialogue_delimiter.py"
 
 GET_TIMESTEPS_SCRIPT="./timestep_parsing.py"
 
@@ -22,7 +23,7 @@ mkdir -p "$OUTPUT_PROCESSED_TEXT"
 for file in "$PATH_TO_CALLHOME_TEXT"/*; do
     if [ -f "$file" ]; then
         filename=$(basename -- "$file")
-        python "$SCRIPT1" "$file" | python "$SCRIPT2" | perl "$SCRIPT3" | perl "$SCRIPT4" > "$OUTPUT_PROCESSED_TEXT/$filename"
+        python "$SCRIPT1" "$file" | python "$SCRIPT2" | perl "$SCRIPT3" | perl "$SCRIPT4" | python "$SCRIPT5" > "$OUTPUT_PROCESSED_TEXT/$filename"
         python "$SCRIPT1" "$file" | python "$GET_TIMESTEPS_SCRIPT" > "$OUTPUT_PROCESSED_TEXT/${filename}_timestamps.txt"
     fi
 done
--- src/m4st/translate/model.py
+++ src/m4st/translate/model.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 from m4st.ollama.client import generate
 
@@ -19,6 +20,44 @@ class TranslationModel(ABC):
     @abstractmethod
     def __call__(self, text: str):
         pass
+
+
+class NLLBTranslateModel(TranslationModel):
+    def __init__(
+        self,
+        source_lang_iso: str,
+        target_lang_iso: str,
+        model_tag: str = "facebook/nllb-200-distilled-600M",
+    ):
+
+        self.language_iso_to_nllb = {
+                "eng" : "eng_Latn",
+                "spa" : "spa_Latn"
+        }
+
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_tag)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_tag)
+
+        self.supported_languages: list[str] = ["eng", "spa"]
+        assert target_lang_iso in self.supported_languages, "NLLB \
+                Actually supports 200 languages, but this model only \
+                expects to work on Spanish and English."
+        assert source_lang_iso in self.supported_languages, "NLLB \
+                Actually supports 200 languages, but this model only \
+                expects to work on Spanish and English."
+
+        self.source_lang_iso: str = source_lang_iso
+        self.target_lang_iso: str = target_lang_iso
+
+    def __call__(self, text: str):
+        inputs = self.tokenizer(text, return_tensors="pt")
+        # NOTE There used to be a max_length=30 parameter. I'm removing it, but
+        # keep in mind this is a sentence-level translation model.
+        # TODO Do you explicitly set the input language?
+        translated_tokens = self.model.generate(
+            **inputs, forced_bos_token_id=self.tokenizer.convert_tokens_to_ids(self.language_iso_to_nllb[self.target_lang_iso]))
+
+        return self.tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
 
 
 class T5TranslateModel(TranslationModel):
